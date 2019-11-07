e4cdd701e194d439ebb2c51d32d5db75c7974170
Malverick <jamesfirth95@yahoo.co.uk>
2019-11-07 11:19:41 +0000

    first commit

---
+++ pyweb/python-server-2.7.15.py
@@ -0,0 +1,9 @@
+# only workswith Python 2
+
+import SimpleHTTPServer
+import SocketServer
+
+PORT = 9000
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+httpd.serve_forever()
